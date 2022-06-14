74e6b583b1f8bc8e412b78078e8a2e5128041b6a
Isabel Portuguez Calderón <isabel210399@gmail.com>
2022-06-14 15:26:23 -0600

    Finish ETL.

--- PythonETL/etl.py
+++ PythonETL/etl.py
@@ -33,63 +33,72 @@ def getProducts(cursorPosgreSQL):
     cursorPosgreSQL.execute(query)
     products = cursorPosgreSQL.fetchall()
 
-    with open('products.csv', 'w') as csvProducts:
-        fieldnames = ['Product_Id', 'Product_Name', 'Product_Price']
-        writer = csv.DictWriter(csvProducts, fieldnames=fieldnames)
-        writer.writeheader()
-        for aux in products:
-            writer.writerow(
-                {'Product_Id': aux[0], 'Product_Name': aux[1], 'Product_Price': aux[2]})
+    # with open('products.csv', 'w') as csvProducts:
+    #     fieldnames = ['Product_Id', 'Product_Name', 'Product_Price']
+    #     writer = csv.DictWriter(csvProducts, fieldnames=fieldnames)
+    #     writer.writeheader()
+    #     for aux in products:
+    #         writer.writerow(
+    #             {'Product_Id': aux[0], 'Product_Name': aux[1], 'Product_Price': aux[2]})
+    #         print("Product_Id: "+ str(aux[0]) +", Product_Name: "+ str(aux[1])+ ", Product_Price: "+ str(aux[2]))
+
+    return products
 
 
 def getPaymentMethod(cursorPosgreSQL):
-    query = "SELECT I.Invoice_Id, PM.Payment_Method_Id, PM.Payment_Method_Name FROM SALES.tb_Invoice AS I JOIN SALES.tb_Payment_Method AS PM ON PM.Payment_Method_Id = I.Payment_Method_Id"
+    query = "SELECT Payment_Method_Id, Payment_Method_Name FROM SALES.tb_Payment_Method"
     cursorPosgreSQL.execute(query)
-    sales = cursorPosgreSQL.fetchall()
+    payments = cursorPosgreSQL.fetchall()
 
-    with open('paymentmethod.csv', 'w') as csvSales:
-        fieldnames = ['Invoice_Id', 'Payment_Method_Id', 'Payment_Method']
-        writer = csv.DictWriter(csvSales, fieldnames=fieldnames)
-        writer.writeheader()
-        for aux in sales:
-            writer.writerow(
-                {'Invoice_Id': aux[0], 'Payment_Method_Id': aux[1], 'Payment_Method': aux[2]})
+    # with open('paymentmethod.csv', 'w') as csvSales:
+    #     fieldnames = ['Invoice_Id', 'Payment_Method_Id', 'Payment_Method']
+    #     writer = csv.DictWriter(csvSales, fieldnames=fieldnames)
+    #     writer.writeheader()
+    #     for aux in sales:
+    #         writer.writerow(
+    #             {'Invoice_Id': aux[0], 'Payment_Method_Id': aux[1], 'Payment_Method': aux[2]})
+
+    return payments
 
 
 def getInvoicesDate(cursorPosgreSQL):
     query = "SELECT Invoice_Id, Purchase_Date FROM SALES.tb_Invoice"
     cursorPosgreSQL.execute(query)
-    dates = cursorPosgreSQL.fetchall()
+    invoicedates = cursorPosgreSQL.fetchall()
 
-    with open('invoicesdates.csv', 'w') as csvDates:
-        fieldnames = ['Invoice_Id',
-                      'Purchase_Date', 
-                      'Purchase_Year', 
-                      'Purchase_Month']
-        writer = csv.DictWriter(csvDates, fieldnames=fieldnames)
-        writer.writeheader()
-        for aux in dates:
-            writer.writerow(
-                {'Invoice_Id': aux[0], 'Purchase_Date': aux[1], 'Purchase_Year': aux[1].strftime('%Y'), 'Purchase_Month': aux[1].strftime('%m')})
+    # with open('invoicesdates.csv', 'w') as csvDates:
+    #     fieldnames = ['Invoice_Id',
+    #                   'Purchase_Date', 
+    #                   'Purchase_Year', 
+    #                   'Purchase_Month']
+    #     writer = csv.DictWriter(csvDates, fieldnames=fieldnames)
+    #     writer.writeheader()
+    #     for aux in dates:
+    #         writer.writerow(
+    #             {'Invoice_Id': aux[0], 'Purchase_Date': aux[1], 'Purchase_Year': aux[1].strftime('%Y'), 'Purchase_Month': aux[1].strftime('%m')})
+
+    return invoicedates
 
 
 def getFactTableInvoiceData(cursorPosgreSQL):
     query = "SELECT I.Invoice_Id, I.Payment_Method_Id, IDT.Product_Id, IDT.Product_Amount, IDT.SubTotal_Price, I.Total_Price FROM SALES.tb_Invoice AS I INNER JOIN SALES.tb_Invoice_Details AS IDT ON I.Invoice_Id = IDT.Invoice_Id"
     cursorPosgreSQL.execute(query)
-    facts = cursorPosgreSQL.fetchall()
+    factsinvoice = cursorPosgreSQL.fetchall()
 
-    with open('factsinvoice.csv', 'w') as csvFacts:
-        fieldnames = ['Id', 'Invoice_Purchase_Date_Id', 'Payment_Method_Id', 'Product_Id', 
-                      'Product_Amount', 'SubTotal_Price', 'Total_Price']
-        writer = csv.DictWriter(csvFacts, fieldnames=fieldnames)
+    # with open('factsinvoice.csv', 'w') as csvFacts:
+    #     fieldnames = ['Id', 'Invoice_Purchase_Date_Id', 'Payment_Method_Id', 'Product_Id', 
+    #                   'Product_Amount', 'SubTotal_Price', 'Total_Price']
+    #     writer = csv.DictWriter(csvFacts, fieldnames=fieldnames)
 
-        writer.writeheader()
-        count = 0
-        for aux in facts:
-            writer.writerow(
-                {'Id': count, 'Invoice_Purchase_Date_Id': aux[0], 'Payment_Method_Id': aux[1], 'Product_Id': aux[1],
-                 'Product_Amount': aux[2], 'SubTotal_Price': aux[3], 'Total_Price': aux[4]})
-            count += 1
+    #     writer.writeheader()
+    #     count = 0
+    #     for aux in facts:
+    #         writer.writerow(
+    #             {'Id': count, 'Invoice_Purchase_Date_Id': aux[0], 'Payment_Method_Id': aux[1], 'Product_Id': aux[2],
+    #              'Product_Amount': aux[3], 'SubTotal_Price': aux[4], 'Total_Price': aux[5]})
+    #         count += 1
+
+    return factsinvoice
 
 
 def createTables(cursorSQLServer):
@@ -98,35 +107,42 @@ def createTables(cursorSQLServer):
     print("Create table tb_DIM_Product, tb_DIM_Payment_Method, tb_DIM_Invoice_Dates, tb_FACT_Table_Invoice successfully")
 
 
-def loadtbDIMProduct(cursorSQLServer):
-    query = "BULK INSERT STOCK.tb_DIM_Product FROM '"+os.path.dirname(os.path.abspath(
-        __file__))+"\\products.csv' WITH (FIRSTROW = 2, FIELDTERMINATOR = ',', ROWTERMINATOR = '\n')"
-    cursorSQLServer.execute(query)
-    cursorSQLServer.commit()
-    print("Product load successfully")
+def loadtbDIMProduct(cursorSQLServer, products):
+    for aux in products:
+        query = 'INSERT INTO STOCK.tb_DIM_PRODUCT VALUES(?,?,?);'
+        cursorSQLServer.execute(
+            query, (aux[0], aux[1], aux[2]))
+    print("Products load successfully")
+    # query = "BULK INSERT STOCK.tb_DIM_Product FROM '"+os.path.dirname(os.path.abspath(
+    #     __file__))+"\\products.csv' WITH (FIRSTROW = 2, FIELDTERMINATOR = ',', ROWTERMINATOR = '\n')"
+    # cursorSQLServer.execute(query)
+    # cursorSQLServer.commit()
+    # print("Product load successfully")
 
 
-def loadtbDIMPaymentMethod(cursorSQLServer):
-    query = "BULK INSERT SALES.tb_DIM_Payment_Method FROM '"+os.path.dirname(os.path.abspath(
-        __file__))+"\\paymentmethod.csv' WITH (FIRSTROW = 2, FIELDTERMINATOR = ',', ROWTERMINATOR = '\n')"
-    cursorSQLServer.execute(query)
-    cursorSQLServer.commit()
+def loadtbDIMPaymentMethod(cursorSQLServer, payment):
+    for aux in payment:
+        query = 'INSERT INTO SALES.tb_DIM_PAYMENT_METHOD VALUES(?,?);'
+        cursorSQLServer.execute(
+            query, (aux[0], aux[1]))
     print("Payment Method load successfully")
 
 
-def loadtbDIMInvoiceDates(cursorSQLServer):
-    query = "BULK INSERT SALES.tb_DIM_Invoice_Dates FROM '"+os.path.dirname(os.path.abspath(
-        __file__))+"\\invoicesdates.csv' WITH (FIRSTROW = 2, FIELDTERMINATOR = ',', ROWTERMINATOR = '\n')"
-    cursorSQLServer.execute(query)
-    cursorSQLServer.commit()
+def loadtbDIMInvoiceDates(cursorSQLServer, invoicedates):
+    for aux in invoicedates:
+        query = 'INSERT INTO SALES.tb_DIM_INVOICE_DATE VALUES (?,?,?,?)'
+        cursorSQLServer.execute(
+            query, (aux[0], aux[1], aux[1].strftime('%Y'), aux[1].strftime('%m')))
     print("Invoice Date load successfully")
 
 
-def loadtbDIMFactTableInvoice(cursorSQLServer):
-    query = "BULK INSERT SALES.tb_FACT_Table_Invoice FROM '"+os.path.dirname(os.path.abspath(
-        __file__))+"\\factsinvoice.csv' WITH (FIRSTROW = 2, FIELDTERMINATOR = ',', ROWTERMINATOR = '\n')"
-    cursorSQLServer.execute(query)
-    cursorSQLServer.commit()
+def loadtbDIMFactTableInvoice(cursorSQLServer, factsinvoice):
+    cont = 1
+    for aux in factsinvoice:
+        query = 'INSERT INTO SALES.tb_FACT_TABLE_INVOICE VALUES (?, ?, ?, ?, ?, ?, ?)'
+        cursorSQLServer.execute(
+            query, (cont, aux[0], aux[1], aux[2], aux[3], aux[4], aux[5]))
+        cont += 1
     print("Fact Table Invoice load successfully")
 
 
@@ -136,10 +152,10 @@ try:
     with connectionPostgreSQL.cursor() as cursorPosgreSQL:
         print(
             "\n*****************************\n Extract data in progress...\n*****************************\n")
-        getProducts(cursorPosgreSQL)
-        getPaymentMethod(cursorPosgreSQL)
-        getInvoicesDate(cursorPosgreSQL)
-        getFactTableInvoiceData(cursorPosgreSQL)
+        prod = getProducts(cursorPosgreSQL)
+        payment = getPaymentMethod(cursorPosgreSQL)
+        invdate = getInvoicesDate(cursorPosgreSQL)
+        factinv = getFactTableInvoiceData(cursorPosgreSQL)
 
         #connectionSQLServer = connectSQLServer()
         connectionSQLServer = connectToLocalSQLServer()
@@ -151,10 +167,10 @@ try:
 
             print(
                 "\n*****************************\n Load data in progress...\n*****************************\n")
-            loadtbDIMProduct(cursorSQLServer)
-            loadtbDIMPaymentMethod(cursorSQLServer)
-            loadtbDIMInvoiceDates(cursorSQLServer)
-            loadtbDIMFactTableInvoice(cursorSQLServer)
+            loadtbDIMProduct(cursorSQLServer, prod)
+            loadtbDIMPaymentMethod(cursorSQLServer, payment)
+            loadtbDIMInvoiceDates(cursorSQLServer, invdate)
+            loadtbDIMFactTableInvoice(cursorSQLServer, factinv)
 
             print(
                 "\n*****************************\n Load data successfully!!\n*****************************\n")
